953f5b56feb0233b740655fea9e8d7fd9abdf1e9
Amit Mangal <mangal.kumar.amit@gmail.com>
2021-01-28 00:34:18 +0530

    war file generated

--- src/main/java/com/beautician/app/BeauticianApplication.java
+++ src/main/java/com/beautician/app/BeauticianApplication.java
@@ -2,13 +2,20 @@ package com.beautician.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import com.beautician.app.security.AppProperties;
 
 @SpringBootApplication
-public class BeauticianApplication {
+public class BeauticianApplication extends SpringBootServletInitializer{
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(BeauticianApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BeauticianApplication.class, args);
